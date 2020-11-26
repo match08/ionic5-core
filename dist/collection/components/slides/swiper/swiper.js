34a047dc1a6119b2a8ab44798aa9f43ecf4c203c
match08 <match_2006@qq.com>
2020-11-26 17:17:02 +0800

    public dist version 5.51

---
+++ dist/collection/components/slides/swiper/swiper.js
@@ -0,0 +1,4 @@
+import { Autoplay, Pagination, Scrollbar, Swiper, Keyboard, Zoom } from 'swiper/swiper.esm';
+
+Swiper.use([Pagination, Scrollbar, Autoplay, Keyboard, Zoom]);
+export { Swiper };
---
+++ dist/types/components/slides/swiper/swiper-interface.d.ts
@@ -0,0 +1,1 @@
+export { Swiper as SwiperInterface, SwiperOptions } from 'swiper';
